9912101a48386eb94f58fb35b6a57a0775a32eb9
AdityaShah7849 <adityashah7849@gmail.com>
2024-05-29 18:45:35 +0530

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,93 @@
+import 'package:adv_basics/Models/quiz_quetions.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  QuizQuestion('How are Flutter UIs built?', [
+    'By combining widgets in code',
+    'By combining widgets in a visual editor',
+    'By defining widgets in config files',
+    'By using XCode for iOS and Android Studio for Android',
+  ]),
+  QuizQuestion(
+    'What\'s the purpose of a StatefulWidget?',
+    [
+      'Update UI as data changes',
+      'Update data as UI changes',
+      'Ignore data changes',
+      'Render UI that does not depend on data',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'What happens if you change data in a StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+  QuizQuestion(
+    'How should you update data inside of StatefulWidgets?',
+    [
+      'By calling setState()',
+      'By calling updateData()',
+      'By calling updateUI()',
+      'By calling updateState()',
+    ],
+  ),
+];
+/*
+
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+    "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "What command do you use to output data to the screen?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answer_index": 2,
+  },
+];
+*/
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:adv_basics/Pages.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/QuestionScreen.dart';
+
+// This widget is the root of your application.
+void main() {
+  runApp(
+    const Quiz(),
+  );
+}
